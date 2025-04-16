dbaf8009d1b054dd56c5ad2483870ab070a0975e
laje-E <136092273+laje-E@users.noreply.github.com>
2025-04-16 09:01:18 -0300

    Add files via upload

---
+++ puntero_6.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+void crear_palabra (char palabra1 [20], char palabra2 [20]);
+void ocurrencias (char palabra1 [20], char palabra2 [20], int n, int m);
+int main () {
+    char palabra1 [20];
+    char palabra2 [20];
+    int n = strlen (palabra1);
+    int m = strlen (palabra2);
+    crear_palabra (palabra1, palabra2);
+    
+    ocurrencias (palabra1, palabra2, n, m);
+}
+
+void crear_palabra (char palabra1 [20], char palabra2 [20]){
+    printf ("ingrese la primer palabra ");
+    scanf ("%s", palabra1);
+    printf ("ingrese la segunda palabra ");
+    scanf ("%s", palabra2);
+}
+
+void ocurrencias (char palabra1 [20], char palabra2 [20], int n, int m) {
+    for (int i=0;i<n;i++){
+        for (int j=0;j<m;j++){
+            strcmp(palabra1 [i], palabra2 [j]);
+            if (strcmp(palabra1 [i], palabra2 [i]) == 0){
+                int cant_ocurrencias = cant_ocurrencias + 1;
+            }
+        }
+    }
+    printf ("la cantidad de ocurrencias es: %d", )
+}
